993247e915f0f658cfa888fdb8b3234e0e9035cb
Tech Support India <tchspprtndindia@gmail.com>
2025-11-03 06:31:39 -0800

    Fixed Next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
- images: {
-    domains: ['127.0.0.1', 'localhost'], 
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: '**', 
+      },
+    ],
+    unoptimized: process.env.NODE_ENV === 'development',
   },
 };
 
